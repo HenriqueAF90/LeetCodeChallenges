28368d1e58ff398f31207013b8ecb6e11993ec16
Henrique BH <37003470+HenriqueAF90@users.noreply.github.com>
2024-07-27 20:29:32 +0000

    repetição com while e operador ternario

--- Java/AdivinhaNumeroGame.java
+++ Java/AdivinhaNumeroGame.java
@@ -1,22 +1,32 @@
  package Java;
 
+import java.util.Random;
 import java.util.Scanner;
 
 class AdivinhaNumeroGame {
     public static void main(String[] args) {
         System.out.println("Bem vindo ao jogo da adivinhação");
-        int numeroSorteio = 10;
-
+        Random random = new Random();
+        int numeroSorteio = random.nextInt(100);
+        int chute = 0;
+        int tentativas = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Qual o número você acha que o computador pensou? ");
-        int chute = sc.nextInt();
 
-        if (chute == numeroSorteio) {
-            System.out.println("Numero correto");
+        while (chute != numeroSorteio) {
+            System.out.println("Qual o número você acha que o computador pensou? [0-100] ");
+            chute = sc.nextInt();
+    
+            tentativas ++;
+            if (chute > numeroSorteio) {
+                System.out.println("O nímero secreto é menor");
+            }
+            else if (chute < numeroSorteio) {
+                System.out.println("O número secreto é maior");   
+            }    
         }
-        else{
-            System.out.println("Infelizmente você errou");
-        }
+        String palavra_tentativas = tentativas > 1 ? " vezes" : " vez";
+
+        System.out.println("Você acertou tentando " + tentativas + palavra_tentativas);
 
         sc.close();
 
